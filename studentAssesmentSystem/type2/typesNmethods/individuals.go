d2d2aaff1b73e32434b602f6041dda3bfdfad8c2
micsamuel <mykelsown@gmail.com>
2026-06-01 17:52:20 +0100

    Implementing the increametation of the TA score due amount of session led

--- studentAssesmentSystem/type2/typesNmethods/individuals.go
+++ studentAssesmentSystem/type2/typesNmethods/individuals.go
@@ -24,6 +24,15 @@ func (std Student) String() string {
 	return fmt.Sprintf("[ %s ] %s · %s · %s %.1f%", std.EnrollmentID, std.Name, std.Course, bar, percent)
 }
 
+func (TA TeachingAssistant) String() string {
+	for i:= 0; i>TA.SessionLed; i++{
+		
+	}
+
+	bar, percent := progressBar(TA.Scores)
+	return fmt.Sprintf("[ %s ] %s · %s · %s %.1f% [+%d sessions]", TA.EnrollmentID, TA.Name, TA.Course, bar, percent, TA.SessionLed)
+}
+
 func progressBar(score []float64) (string, float64) {
 	bar := "░░░░░░░░░░"
 	numScore := len(score) - 1
