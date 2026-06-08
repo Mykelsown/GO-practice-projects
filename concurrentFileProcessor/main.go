cd343a4b6b7b5969672b25da2115dddf6f648e67
micsamuel <mykelsown@gmail.com>
2026-06-08 16:01:34 +0100

    Done almost all that is required, what is remainig is to log the output to the console in a formatted way

--- concurrentFileProcessor/main.go
+++ concurrentFileProcessor/main.go
@@ -3,11 +3,35 @@ package main
 import (
 	MTR "CFP/methodsTypesAndRoutines"
 	"fmt"
+	"sync"
 )
 
 func main() {
 	fileLogs := []string{"Data.txt", "Network.txt", "Running.txt", "Clean-error.txt", "Append.txt", "Query-error.txt"}
-	bufChan := make(chan string,  len(fileLogs))
+	bufChan := make(chan MTR.LogResult, len(fileLogs)) // Helps strore certain amount of data, regardless if there's a receiver to collect or not.
+	var wg sync.WaitGroup
+
+	for _, fileName := range fileLogs {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			bufChan <- MTR.ProcessLog(fileName)
+		}(&wg)
+	}
+
+	wg.Wait()
+	close(bufChan)
+
+	successes := make([]MTR.LogResult, 0)
+	failures := make([]MTR.LogResult, 0)
+	for val := range bufChan {
+		if val.HasError {
+			failures = append(failures, val)
+		}
+		successes = append(successes, val)
+	}
+
 	
-	fmt.Println(MTR.ProcessLog("nERRORw.log"))
+
+	fmt.Println()
 }
--- concurrentFileProcessor/methodsTypesAndRoutines/routines.go
+++
@@ -1,3 +0,0 @@
-package MTR
-
-func 
