90005aec999247dbba6881c5b6d6c972e0fb7d84
Mykelsown <mykelsown@gmail.com>
2026-07-05 02:37:20 +0100

    implemented the project for bactracking and fast-forwarding to specific point

--- brainFuck/main.go
+++ brainFuck/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
-	// "os"
+	"os"
 )
 
 func main() {
-	a := []int{0, 1, 2, 3, 4, 5}
-	// b := &a[0]
-	// *b = 10
-	i := 0
-	count := 0
-	for i = 0; i < len(a); i++{
-		if a[i] == 2 && count < 5 {
-			i = 0
-			count++
-		}
-		fmt.Println(i)
-	}
-	// fmt.Println(a)
-	// brainFuck(os.Args[1])
+	// a := []int{0, 1, 2, 3, 4, 5}
+	// i := 0
+	// count := 0
+	// for i = 0; i < len(a); i++{
+	// 	if a[i] == 2 && count < 5 {
+	// 		i = 0
+	// 		count++
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	brainFuck(os.Args[1])
 }
 
 func brainFuck(code string) {
@@ -34,14 +32,20 @@ func brainFuck(code string) {
 		revCommand bool
 	)
 
-	for _, c := range code {
-		if ffCommand && revCommand {
-			continue
-		} else if ffCommand && c == ']' {
-			ffCommand = false
+	openBracketPosition := make([]int, 0)
+	openBracketPosition = append(openBracketPosition, 0)
+	OBC := 0
+
+	i := 0
+	for i = 0 ; i < len(code); i++ {
+		// For the backtracking logic
+		if revCommand {
+			i = openBracketPosition[OBC] + 1
+			revCommand = false
+			OBC--
 		}
 			
-		switch c {
+		switch code[i]{
 		case '>':
 			movementCount++
 			pointBytes[movementCount] = *reference
@@ -58,16 +62,27 @@ func brainFuck(code string) {
 			println(*reference, "m")
 		case '.':
 			out += string(*reference)
+			*reference = 0
 		case '[':
 			if pointBytes[movementCount] == 0{
 				ffCommand = true
+				openBracketPosition = append(openBracketPosition, i)
+				OBC++
 			}
 		case ']':
-			if pointBytes[movementCount] == 0{
+			if pointBytes[movementCount] != 0{
 				revCommand = true
 			}
 		default:
 			fmt.Println("foreign operator is present in the code string")
+		}
+
+		// The condition helps satisfy the criteria for skipping all operators until it gets to ] operator
+		if ffCommand {
+			continue
+		} else if ffCommand && code[i]== ']' {
+			ffCommand = false
+			continue
 		}
 	}
 	fmt.Println(*reference)
